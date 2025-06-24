d82ebf987d21fa26461a4437c21b7a635cc4c3a1
smbraden <bradensonja08@gmail.com>
2025-06-24 10:29:17 -0700

    created assignment1

---
+++ assignment1/set.js
@@ -0,0 +1,30 @@
+class Set {
+    constructor(my_set) {
+        this.set = my_set;
+    }
+
+    add(value) {
+        //Insert the value into the Set, if it does not already exist. 
+    }
+
+    has(value) {
+        //Return true or false if the Set contains the given value. 
+    }
+
+    delete(value) {
+        //Delete the given value from the set. 
+        //Return true if the value was deleted, or false if the value did not exist in the set.  
+    }
+
+    clear() {
+        //Remove all values from the set. 
+    }
+
+    values() {
+        //Return an array of all values in the set. 
+    }
+
+    size() {
+        //Return the amount of values in the set. 
+    }
+}
