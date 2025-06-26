e2f0f4522279823cb08500102e04c5d12c76fd5c
smbraden <bradensonja08@gmail.com>
2025-06-26 12:29:51 -0700

    changes to test file, and adding sonja's set class file

---
+++ assignment1/set_test.js
@@ -0,0 +1,58 @@
+import { Set } from './set.js';
+
+const mySet = new Set([]);
+let myValues = [1, 1, 2, 5, 6, 6, 7, 9, 9];
+
+
+// test values() and size()
+console.log("\nBEFORE additions...");
+console.log("mySet.values() --> " + mySet.values());
+console.log("mySet.size() --> " + mySet.size() + "\n");
+
+// test add()
+for (let i = 0; i < myValues.length ; i++){
+    mySet.add(myValues[i]);
+    console.log("mySet.add(" + myValues[i] + ") --> " + mySet.values());
+}
+
+console.log("\nAFTER additions...");
+console.log("mySet.values() --> " + mySet.values());
+console.log("mySet.size() --> " + mySet.size());
+console.log("\n------------------------------- \n");
+
+console.log("Testing has() method...");
+console.log("mySet: " + mySet.values()  + "\n");
+for (let i = 0; i < 11 ; i++){
+    console.log("mySet.has(" + i + ") --> " + mySet.has(i));
+}
+console.log("\n------------------------------- \n");
+
+
+// test clear()
+console.log("Testing clear() method... \n");
+console.log("BEFORE clear()...");
+console.log("mySet.values() --> " + mySet.values());
+console.log("mySet.size() --> " + mySet.size() + "\n");
+
+mySet.clear();
+
+console.log("AFTER clear()...");
+console.log("mySet.values() --> " + mySet.values());
+console.log("mySet.size() --> " + mySet.size());
+console.log("\n------------------------------- \n");
+
+// test delete()
+for (let i = 0; i < myValues.length ; i++){
+    mySet.add(myValues[i]);
+}
+
+console.log("Testing delete() method...");
+console.log("mySet: " + mySet.values()  + "\n");
+
+let hadValue = false;
+
+for (let i = 0; i < 11 ; i++){
+    hadValue = mySet.delete(i);
+    console.log("mySet.delete(" + i + ") --> " + hadValue + " " + mySet.values());
+}
+console.log("\n------------------------------- \n");
